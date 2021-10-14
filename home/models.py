0e42758c67ac8b81eeb22ce54037f8e3dc7c0f71
pablopalillo <pablo.576@hotmail.com>
2021-10-14 00:53:06 -0500

    CTA section on Home Page

--- home/models.py
+++ home/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from wagtail.core.models import Page
 from wagtail.core import blocks
-from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.fields import StreamField
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+
+
+class CTABlock(blocks.StructBlock):
+
+    title = blocks.CharBlock(required=True, max_length=60)
+    text = blocks.CharBlock(max_length=100)
+    button_page = blocks.PageChooserBlock()
+    button_text = blocks.CharBlock(required=True)
+
+    class Meta:
+        template = "streams/blocks/cta_block.html"
+        icon = "placeholder"
+        label = "Call To Action"
 
 
 class HomePage(Page):
@@ -14,7 +27,9 @@ class HomePage(Page):
                      ('content', blocks.RichTextBlock()),
                      ('img', ImageChooserBlock(required=False)),
                     ], template='streams/blocks/section.html',  icon='user')
-         )
+
+         ),
+        ('cta', CTABlock())
     ])
 
     content_panels = Page.content_panels + [
